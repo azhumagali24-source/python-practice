84a176b412a2b013353ab0a66299a21f95ebda26
azhumagali24-source <a.zhumagali24@apec.edu.kz>
2026-04-16 14:09:21 +0500

    Update practice.py

--- practice.py
+++ practice.py
@@ -24,11 +24,11 @@ s = input()
 count = 1
 result = ""
 
-for i in range(len(s) - 1):
-    if s[i] == s[i + 1]:
+for i in range(1,len(s):
+    if s[i] == s[i - 1]:
         count += 1
     else:
-        result += s[i] + str(count)
+        result += s[i-1] + str(count)
         count = 1
 
 result += s[-1] + str(count)
